389b7b7319ce8b048ba64c68eda3b4eec8e4c946
minsun24 <jms49526@gmail.com>
2023-09-20 01:18:14 +0900

    요세푸스 순열

---
+++ week2/B20301.py
@@ -0,0 +1,22 @@
+# 반전 요세푸스 순열 
+from sys import stdin  # 빠른 입출력
+from collections import deque # 덱 모듈
+
+input = stdin.readline
+
+N, K, M = map(int, input().split())
+
+
+q = deque(range(1, N+1))
+
+index_ = 0
+
+while q:
+    if index_ // M % 2 == 0:
+        for _ in range(K-1):
+            q.append(q.popleft())
+    else:
+        for _ in range(K):
+            q.appendleft(q.pop())
+    index_ += 1
+    print(q.popleft())
